dbaace11a0d723f77d0ef78537b3a3d304edbcaa
r-sorna <54575693+r-sorna@users.noreply.github.com>
2019-11-20 12:38:46 +0530

    Update travelcabbook.py

--- travelcabbook.py
+++ travelcabbook.py
@@ -1,19 +1,29 @@
-print("bike=10,car=20,auto=5")
-catagory=input("bike/car/auto: ")
 username=input("name: ")
 mail=input("mailid: ")
 phone=input("phone: ")
+catagory=input("bike/car/auto: ")
 source=input("source: ")
 destination=input("destination: ")
 bprice=10
 cprice=20
 aprice=5
-km=source-destination
-if "-"in source and destination:
+km=0
+if "-"in source or destination:
     print("please enter valid details: ")
-elif source==0 or destination==0:
+elif source==0 and destination==0:
     print("please enter valid details") 
-elif catagory=="bike":
+elif source.isalpha() and destination.isalpha():
+    print("please enter valid details") 
+elif source>destination:
+    source1=int(source)
+    dest=int(destination)
+    km=destination-source
+else:
+    source1=int(source)
+    dest=int(destination) 
+    km=source-destination   
+print("calculation")           
+if catagory=="bike":
     total=km*bprice
     print(total)
 elif catagory=="car":
