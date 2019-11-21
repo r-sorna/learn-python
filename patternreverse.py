81b0d510538d4430090e3a448d17b01b89d87ca0
r-sorna <54575693+r-sorna@users.noreply.github.com>
2019-11-21 11:09:05 +0530

    Add files via upload

---
+++ patternreverse.py
@@ -0,0 +1,8 @@
+for i in range(0,5):
+  for j in range(0,i+1):
+    print("*",end=" ")
+  print("\r")
+for i in range(0,5):
+  for i in range(0,4-i):
+    print("*",end=" ")
+  print("\r")       
---
+++ userpatternreverse.py
@@ -0,0 +1,6 @@
+k=int(input())
+m=int(input())
+for i in range(k,m):
+  for i in range(k,m-i):
+    print("#",end=" ")
+  print("\r")  
