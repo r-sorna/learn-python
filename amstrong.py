d3fe9cdc6209586ed67dc62e055a9e77f08ad191
r-sorna <54575693+r-sorna@users.noreply.github.com>
2019-11-21 16:26:50 +0530

    Add files via upload

---
+++ amstrong.py
@@ -0,0 +1,10 @@
+s=input()
+k=0
+for i in s:
+  m=int(i)
+  j=m**3
+  k+=j    
+if k==int(s):
+  print("yes")
+else:
+  print("no")  
---
+++ secondlargestno.py
@@ -0,0 +1,8 @@
+a=input()
+lis=[]
+for i in a:
+  k=int(i)
+  lis.append(k)
+lis.sort()
+print(lis[-2])  
+  
